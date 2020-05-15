69ed25d45136284a6a6940dfc3af45d90b141ebf
Max Kalyabin <maksim@kalyabin.ru>
2020-05-15 10:06:12 +0300

    Initial commit

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
+type fxResult struct {
+	num int
+	x int
+}
+
 func Merge2Channels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
-	results := make([]chan int, 2)
-
-	results[0] = make(chan int, 1)
-	results[1] = make(chan int, 1)
-
-	for x := 0; x < n; x++ {
-		go func(x int) {
-			r1 := <- results[0]
-			r2 := <- results[1]
-
-			out <- r1 + r2
-		}(x)
+	processed := 0
+	processFx := func(item fxResult, resultChan chan fxResult) {
+		item.x = f(item.x)
+		processed++
+		resultChan <- item
 	}
 
 	go func() {
+		results := make([][]int, n)
+		resultChan := make(chan fxResult)
+
+		processed := 0
+		ch1 := 0
+		ch2 := 0
+
 		for {
 			select {
-			case x := <-in1:
-				go func(x int) {
-					results[0] <- f(x)
-				}(x)
-			case x := <-in2:
-				go func(x int) {
-					results[1] <- f(x)
-				}(x)
+			case x := <- in1:
+				if ch1 < n {
+					item := fxResult{ch1, x}
+					ch1++
+					go processFx(item, resultChan)
+				}
+			case x := <- in2:
+				if ch2 < n {
+					item := fxResult{ch2, x}
+					ch2++
+					go processFx(item, resultChan)
+				}
+			case item := <- resultChan:
+				results[item.num] = append(results[item.num], item.x)
+
+				if len(results[item.num]) == 2 {
+					result := results[item.num][0] + results[item.num][1]
+					out <- result
+					processed++
+				}
+
+				if processed == n {
+					return
+				}
 			}
 		}
 	}()
--- main_test.go
+++ main_test.go
@@ -3,33 +3,42 @@ package main
 import (
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestMerge2Channels(t *testing.T) {
 	n := 100
 
+	gotResultCount := 0
 	var x1Input []int
 	var x2Input []int
 	var expectedResults []int
 
 	f := func(i int) int {
-		return i * 2
+		time.Sleep(time.Millisecond * 1)
+		return i * i
 	}
 
 	for x := 0; x < n; x++ {
-		x1Input = append(x1Input, x + 1)
-		x2Input = append(x2Input, x + 2)
-		expectedResults = append(expectedResults, f(x + 1) + f(x + 2))
+		x1 := x + 123
+		x2 := x + 456
+
+		x1Input = append(x1Input, x1)
+		x2Input = append(x2Input, x2)
+
+		expectedResults = append(expectedResults, f(x1) + f(x2))
 	}
 
-	in1 := make(chan int)
-	in2 := make(chan int)
+	t.Logf("Wait Result count: %d\n", len(expectedResults))
+
+	in1 := make(chan int, 1)
+	in2 := make(chan int, 1)
 
 	out := make(chan int)
 
 	Merge2Channels(f, in1, in2, out, n)
 
-	var wg sync.WaitGroup
+	wg := &sync.WaitGroup{}
 
 	wg.Add(2)
 
@@ -39,24 +48,28 @@ func TestMerge2Channels(t *testing.T) {
 			in2 <- x2Input[x]
 		}
 
+		in1 <- 10
+		in2 <- 20
+
 		wg.Done()
 	}()
 
 	go func() {
 		for x := 0; x < n; x++ {
 			result := <- out
-
 			gotValue := false
+			gotResultCount++
 
 			for k, v := range expectedResults {
 				if result == v {
 					gotValue = true
 					expectedResults = append(expectedResults[:k], expectedResults[k+1:]...)
+					t.Logf("Got expected result: %d", result)
 				}
 			}
 
 			if !gotValue {
-				t.Errorf("[%d] Result %d not found", x, result)
+				t.Errorf("[%d] Result %d not found", n, result)
 			}
 		}
 
@@ -64,4 +77,11 @@ func TestMerge2Channels(t *testing.T) {
 	}()
 
 	wg.Wait()
+
+	t.Logf("Got result count: %d", gotResultCount)
+	t.Logf("Got expected results: %d", len(expectedResults))
+
+	close(in1)
+	close(in2)
+	close(out)
 }
